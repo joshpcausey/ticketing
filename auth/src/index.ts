66f1e7236a60587f9364fd3c242eb435d1e5d631
Joshua Causey <joshpcausey@gmail.com>
2024-12-13 20:14:36 -0600

    add validation to inputs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("hello world");
-});
+app.use(currentUserRouter, signinRouter, signoutRouter, signupRouter);
 
 app.listen(3000, () => {
   console.log("auth listening on port 3000");
