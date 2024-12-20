5ae9e3d0e3ec0a966cd5f72920b7317ae1ee4e6b
Joshua Causey <joshpcausey@gmail.com>
2024-12-20 10:53:21 -0600

    add signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email not valid'),
+    body('password').trim().notEmpty().withMessage('Must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
