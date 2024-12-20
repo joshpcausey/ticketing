f00be7be83f830e5bd5bd1609545e87656cc6f36
Joshua Causey <joshpcausey@gmail.com>
2024-12-20 12:13:02 -0600

    error checking

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -10,12 +10,8 @@ router.post(
     body('email').isEmail().withMessage('Email not valid'),
     body('password').trim().notEmpty().withMessage('Must supply a password'),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
+  validateRequest,
+  (req: Request, res: Response) => {}
 );
 
 export { router as signinRouter };
