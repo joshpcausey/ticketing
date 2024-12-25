c40c3a177b4c8267908c1be20d5fbc271e04ac96
Joshua Causey <joshpcausey@gmail.com>
2024-12-25 14:42:08 -0600

    added current user to hompage and passing cookie around

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const Landing = () => {
+import axios from 'axios';
+
+const Landing = (props) => {
+  console.log(props);
   return <h1>Landing Page</h1>;
+};
+
+export const getServerSideProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios
+      .get(
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+
+    return { props: data };
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return { props: data };
+  }
 };
 
 export default Landing;
