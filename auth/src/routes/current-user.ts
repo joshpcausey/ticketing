e3626bc15f0ee4a05f3871cdd6fb7f58b44c9642
Joshua Causey <joshpcausey@gmail.com>
2024-12-26 09:20:19 -0600

    remove unused import

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 
 import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
