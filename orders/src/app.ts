4cbd8af4167c95291143c4fe293886591e9fe3cc
Joshua Causey <joshpcausey@gmail.com>
2025-01-20 06:44:51 -0600

    add orders skeleton

---
+++ orders/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { showTicketRouer } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+import cookieSession from 'cookie-session';
+
+import {
+  NotFoundError,
+  currentUser,
+  errorHandler,
+} from '@joshuacauseytickets/common';
+import { createTicketRouter } from './routes/new';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(
+  createTicketRouter,
+  showTicketRouer,
+  indexTicketRouter,
+  updateTicketRouter
+);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
