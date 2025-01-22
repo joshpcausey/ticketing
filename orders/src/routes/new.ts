e0c0a8d196b12451efdd95860c821e7096f6d49d
Joshua Causey <joshpcausey@gmail.com>
2025-01-22 08:20:08 -0600

    add stuff for mongo

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@joshuacauseytickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // can remove this check which is lightly coupling our orders service to the ticket service
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('ticketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
