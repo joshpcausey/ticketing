1de8999386d75838b7cdd377f9b13c7daf8e25e7
Joshua Causey <joshpcausey@gmail.com>
2025-02-06 19:08:43 -0600

    updatess

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -1,9 +1,15 @@
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listener {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
@@ -28,7 +34,7 @@ export abstract class Listener {
     );
 
     subscription.on('message', (msg: Message) => {
-      console.log(`Message recieved: ${this.subject} / ${this.queueGroupName}`);
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 
       const parsedData = this.parseMessage(msg);
       this.onMessage(parsedData, msg);
@@ -39,6 +45,6 @@ export abstract class Listener {
     const data = msg.getData();
     return typeof data === 'string'
       ? JSON.parse(data)
-      : JSON.parse(data.toString('utf-8'));
+      : JSON.parse(data.toString('utf8'));
   }
 }
